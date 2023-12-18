491c2c6e28a55d256d2fdca99a05848b23223881
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2023-12-18 12:39:17 +0800

    invoice can give receipt

--- src/app/hotelbooking/system/Invoice.java
+++ src/app/hotelbooking/system/Invoice.java
@@ -9,7 +9,90 @@ public class Invoice {
     private LocalDate startBookingDate;
     private Duration bookingDuration;
     private boolean isPaid;
+    private String paidWith;
+    private String ccNo;
 
+    public String getReceipt() {
+
+        if (!this.isPaid) {
+            return "This invoice has not been paid yet.";
+        }
+        
+
+        if (paidWith.equals("cash")) {
+            String format = "RECEIPT\n" +
+                    "=======\n" +
+                    "Date: %s\n" +
+                    "\n" +
+                    "Room: %s\n" +
+                    "Room Price: RM%.2f\n" +
+                    "\n" +
+                    "Booking:\n" +
+                    "  Start Date: %s\n" +
+                    "  Last Date : %s\n" +
+                    "  Duration  : %s\n" +
+                    "\n" +
+                    "Total Price: RM%.2f\n" +
+                    "\n" +
+                    "Paid with: %s\n";
+
+            return String.format(format, 
+                this.invoiceDate,
+                this.theRoomToBeBooked,
+                this.theRoomToBeBooked.getPrice(),
+                this.startBookingDate,
+                this.startBookingDate.plusDays(this.bookingDuration.toDays()-1),
+                this.bookingDuration.toDays(),
+                this.theRoomToBeBooked.getPrice() * this.bookingDuration.toDays(),
+                "Cash"
+            ).strip();
+        }
+        else if (this.paidWith.equals("card")) {
+            String format = "RECEIPT\n" +
+            "=======\n" +
+            "Date: %s\n" +
+            "\n" +
+            "Room: %s\n" +
+            "Room Price: RM%.2f\n" +
+            "\n" +
+            "Booking:\n" +
+            "  Start Date: %s\n" +
+            "  Last Date : %s\n" +
+            "  Duration  : %s\n" +
+            "\n" +
+            "Total Price: RM%.2f\n" +
+            "\n" +
+            "Paid with: %s\n" +
+            "Card: %s\n";
+
+            int cardLength = ccNo.length();
+
+            String cardInReceipt = "";
+            for (int i = 0; i < cardLength - 4; i++) {
+                cardInReceipt += "X";
+            }
+            cardInReceipt +=ccNo.substring(ccNo.length() - 4);
+
+            return String.format(format, 
+                this.invoiceDate,
+                this.theRoomToBeBooked,
+                this.theRoomToBeBooked.getPrice(),
+                this.startBookingDate,
+                this.startBookingDate.plusDays(this.bookingDuration.toDays()-1),
+                this.bookingDuration.toDays(),
+                this.theRoomToBeBooked.getPrice() * this.bookingDuration.toDays(),
+                "Card", cardInReceipt).strip();
+        }
+        return "This invoice is being paid by unknown type of payment method.";
+    }
+
+    public String getPaidWith() {
+        return paidWith;
+    }
+
+    public void setPaidWith(String paidWith) {
+        this.paidWith = paidWith;
+    }
 
     public Invoice(LocalDate invoiceDate, Room theRoomToBeBooked, LocalDate startBookingDate,
             Duration bookingDuration) {
@@ -81,5 +164,14 @@ public class Invoice {
 
     public void payByCash() {
         this.setPaid(true);
+        this.paidWith = "cash";
+    }
+
+    public String getCcNo() {
+        return ccNo;
+    }
+
+    public void setCcNo(String ccNo) {
+        this.ccNo = ccNo;
     }
 }
